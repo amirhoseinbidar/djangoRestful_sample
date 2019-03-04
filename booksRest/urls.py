bd6ffbf2b1709155ba60931a36d538836f6dd2f7
amirhosein bidar <amirhoseinbk00@gmail.com>
2019-03-04 23:32:03 +0330

    Update urls.py

--- booksRest/urls.py
+++ booksRest/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url , include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^',include('books.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
